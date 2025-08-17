6ff64cd31fdf84593a5c8b940fe07c240302c545
Abishekdevil <abishekcholan07@gmail.com>
2025-08-17 21:17:41 +0530

    done

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,7 +3,6 @@ import Game from './components/Game';
 import Login from './components/Login';
 import Signup from './components/Signup';
 import History from './components/History';
-import ProtectedRoute from './components/ProtectedRoute';
 
 
 export default function App() {
