e5edb12f03ff13da982c1cda906a304ab870bd25
Abishekdevil <abishekcholan07@gmail.com>
2025-08-18 10:34:05 +0530

    routes changes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -9,7 +9,7 @@ export default function App() {
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<Login />} />
+        <Route path="/login" element={<Login />} />
         <Route path="/signup" element={<Signup />} />
         <Route path="/game" element={<Game />} />
         <Route path="/history" element={<History />} />
